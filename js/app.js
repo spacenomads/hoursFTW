dcf122c6e0871a34da9c0ba323eae10b373db242
Carlos Mañas <carlos@spacenomads.com>
2019-06-09 22:09:04 +0200

    Add new functionality

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 'use strict';
 
 const tsMin = document.querySelector('.js__user-ts');
-const userMin = document.querySelector('.js__fake-field');
 const resultHHMM = document.querySelector('.js__result-hhmm');
 const resultDecimal = document.querySelector('.js__result-decimal');
+const sizer = document.querySelector('.js__field-sizer');
+
+function setFieldWidth(content) {
+  sizer.innerHTML = content;
+  const newSize = sizer.offsetWidth + 'px';
+  tsMin.style.width = newSize;
+}
 
 function getTS(ts) {
   const hours = Math.floor(ts / 60);
@@ -21,19 +27,13 @@ function writeTime(el,content) {
   el.innerHTML = content;
 }
 
-userMin.addEventListener('keyup', event => {
-  const userMinutes = event.currentTarget.innerHTML;
-  tsMin.value = userMinutes.replace(/\D/g,'');
-
-  const fakeEvent = new CustomEvent('change');
-  tsMin.dispatchEvent(fakeEvent);
-});
-
-tsMin.addEventListener('change', event => {
-  const userMinutes = event.currentTarget.value;
+function getTime(event) {
+  const userMinutes = parseInt(event.currentTarget.value) || 0;
   const {hours, minutes} = getTS(userMinutes);
   const decimal = getDecimalTS(userMinutes);
-
+  setFieldWidth(userMinutes);
   writeTime(resultHHMM, `${hours}<span classs="unit">h</span> ${minutes}<span classs="unit">min</span>`);
   writeTime(resultDecimal, `${decimal.toFixed(2)}<span classs="unit">h</span>`);
-});
+}
+
+tsMin.addEventListener('keyup', getTime);
